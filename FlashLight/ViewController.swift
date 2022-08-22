77ecaf96c66aab3c4723e2cf1954bd7466921b39
Julian De La Torre <jujudlt3@gmail.com>
2022-08-22 08:47:05 -0700

    Initial Commit

---
+++ FlashLight/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FlashLight
+//
+//  Created by De La Torre, Julian - Student on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func flashColorButton(_ sender: Any)
+    {
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+    
+}
+
+
